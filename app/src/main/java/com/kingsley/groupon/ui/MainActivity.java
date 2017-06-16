2643d9e633d2040fc4fde7f117c9eb4518207979
KingsleyWu <351054820@qq.com>
2017-06-17 00:34:09 +0800

    Xml搭建

--- app/src/main/java/com/kingsley/groupon/ui/MainActivity.java
+++ app/src/main/java/com/kingsley/groupon/ui/MainActivity.java
@@ -2,8 +2,13 @@ package com.kingsley.groupon.ui;
 
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,6 +21,9 @@ import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.kingsley.groupon.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mMainTvAddress;
@@ -26,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private View mMainMenuLayout;
     private PullToRefreshListView mMainPrListView;
     private ListView mMainListView;
+    private List<String> datas;
+    private ArrayAdapter<String> adapter;
     private TextView mMainMenuTvComment;
     private TextView mMainMenuTvStore;
     private TextView mMainMenuTvScan;
@@ -36,6 +46,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RadioButton mRbMainFooterGroup;
     private RadioButton mRbMainFooterFind;
     private RadioButton mRbMainFooterMy;
+    private View mIconsView;
+    private View mSquareView;
+    private View mAdsView;
+    private View mCategoriesView;
+    private View mRecommendView;
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mMainMenuTvScan = (TextView) findViewById(R.id.main_menu_tv_scan);
         mMainMenuTvPlay = (TextView) findViewById(R.id.main_menu_tv_play);
 
-        mRgMainFooter = (RadioGroup) findViewById(R.id.rg_main_footer);
+        mRgMainFooter = (RadioGroup) findViewById(R.id.main_rg_rooter);
 
         mRbMainFooterHome = (RadioButton) findViewById(R.id.rb_main_footer_home);
         mRbMainFooterGroup = (RadioButton) findViewById(R.id.rb_main_footer_group);
@@ -71,7 +87,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setAdapter() {
-       // mMainListView.setAdapter();
+        datas = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this,R.layout.main_item_layout,datas);
+        addHeaderView();
+        mMainListView.setAdapter(adapter);
+    }
+
+    private void addHeaderView() {
+        inflater = LayoutInflater.from(this);
+        mIconsView = inflater.inflate(R.layout.header_list_icons,mMainListView,false);
+        mSquareView = inflater.inflate(R.layout.header_list_square,mMainListView,false);
+        mAdsView = inflater.inflate(R.layout.header_list_ads,mMainListView,false);
+        mCategoriesView = inflater.inflate(R.layout.header_list_categories,mMainListView,false);
+        mRecommendView = inflater.inflate(R.layout.header_list_recommend,mMainListView,false);
+        initIconsView();
+        mMainListView.addHeaderView(mIconsView);
+        mMainListView.addHeaderView(mSquareView);
+        mMainListView.addHeaderView(mAdsView);
+        mMainListView.addHeaderView(mCategoriesView);
+        mMainListView.addHeaderView(mRecommendView);
+    }
+
+
+    private void initIconsView() {
+        ViewPager viewpager = (ViewPager) mIconsView.findViewById(R.id.vp_header_list_icons);
+        PagerAdapter pagerAdapter = new PagerAdapter() {
+            int[] resIDs = new int[]{
+                    R.layout.icons_list_1,
+                    R.layout.icons_list_2,
+                    R.layout.icons_list_3
+            };
+            @Override
+            public int getCount() {
+                return 60000;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                int layoutId = resIDs[position%resIDs.length];
+                View view =inflater.inflate(layoutId,container,false);
+                container.addView(view);
+                return view;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        };
+        viewpager.setAdapter(pagerAdapter);
+        viewpager.setCurrentItem(30000);
     }
     //设置监听
     private void setListeners() {
@@ -82,7 +152,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mMainPrListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
             @Override
             public void onRefresh(PullToRefreshBase<ListView> refreshView) {
-
+                mMainPrListView.onRefreshComplete();
             }
         });
 
